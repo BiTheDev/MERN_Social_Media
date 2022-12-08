713fbebb1ed7204cb22d7f8908124aed352f06b2
Brian Ho <brian@MacBook-Pro.local>
2022-12-08 10:08:37 -0800

    server side post part1 completed

--- server/index.js
+++ server/index.js
@@ -2,3 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { CONNECTION_URL } from './security/tokens.js';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts',postRoutes);
+
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+
+
+
+const PORT = process.env.PORT || 5050;
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=>app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error)=>console.log(error.message));
+
+//ongoose.set('useFindAndModify',false);
