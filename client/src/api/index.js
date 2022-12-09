b5e10dff9d034f529299b99bd228d9e483350883
Brian Ho <brian@MacBook-Pro.local>
2022-12-09 00:37:43 -0800

    update post completed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,4 +4,6 @@ const url = 'http://localhost:5050/posts';
 
 export const fetchPosts = () => axios.get(url);
 
-export const createPost =(newPost) =>axios.post(url, newPost); 
+export const createPost =(newPost) =>axios.post(url, newPost);
+
+export const updatePost = (id, updatePost)=>axios.patch(`${url}/${id}`, updatePost)
