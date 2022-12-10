82c1fa16c46551ea5718626ac130688b8e3ec794
Brian Ho <brian@MacBook-Pro.local>
2022-12-10 01:51:15 -0800

    user model, controller for login and register completed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import { CONNECTION_URL } from './security/tokens.js';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 
 app.use('/posts',postRoutes);
-
+app.use('/user', userRoutes);
 
 
 const PORT = process.env.PORT || 5050;
